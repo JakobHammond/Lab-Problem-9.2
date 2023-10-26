8294ee9758c9ab502f1f2e66db1b03ce293c391e
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-10-26 12:37:19 -0400

    Completed isSortedIncreasing

--- main.cpp
+++ main.cpp
@@ -11,3 +11,11 @@ using namespace std;
 int main(void){
     
 }
+
+bool isSortedIncreasing(int values[], int size){
+    bool inc = true;
+    for(int i = 1; i < size; i++){
+        if(values[i] < values[i - 1]) inc = false;
+    }
+    return inc;
+}
