b1408589edddc6550b9ebfc4397a7ede708c0d7e
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-10-26 12:58:10 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,7 +8,29 @@
 #include <iostream>
 using namespace std;
 
+bool isSortedIncreasing(int values[], int size);
+bool isSortedDecreasing(int values[], int size);
+bool hasAdjacentDuplicates(int values[], int size);
+bool hasDuplicates(int values[], int size);
 int main(void){
+    int values[6];
+    int size = 6;
+    //take user input
+    for(int i = 0; i < size; i++){
+        cout << "Enter integer #" << i << ": ";
+        cin >> values[i];
+    }
+    //outputs
+    if(isSortedIncreasing(values, size)) cout << "The data are increasing." << endl;
+    else cout << "The data are not increasing." << endl;
+    if(isSortedDecreasing(values, size)) cout << "The data are decreasing." << endl;
+    else cout << "The data are not decreasing." << endl;
+    if(hasAdjacentDuplicates(values, size)) cout << "The data has adjacent duplicates." << endl;
+    else cout << "The data does not have adjacent duplicates." << endl;
+    if(hasDuplicates(values, size)) cout << "The data has duplicates." << endl;
+    else cout << "The data does not have duplicates.";
+    return 0;
+    
     
 }
 
@@ -20,7 +42,7 @@ bool isSortedIncreasing(int values[], int size){
     return inc;
 }
 
-bool isSortedDec(int values[], int size){
+bool isSortedDecreasing(int values[], int size){
     bool dec = true;
     for(int i = 1; i < size; i++){
         if(values[i] > values[i - 1]) dec = false;
@@ -28,7 +50,7 @@ bool isSortedDec(int values[], int size){
     return dec;
 }
 
-bool hasAdjecentDuplicates(int values[], int size){
+bool hasAdjacentDuplicates(int values[], int size){
     bool adjacentVal = false;
     for(int i = 0; i < size; i++){
         if(values[i] == values[i + 1]) adjacentVal = true;
@@ -37,11 +59,11 @@ bool hasAdjecentDuplicates(int values[], int size){
 }
 
 bool hasDuplicates(int values[], int size){
-    bool dupe = false;
+    bool duplicates = false;
     for(int i = 0; i < size; i++){
-        for(int j = i + 1; i < size; j++){
-            if(values[i] == values[j]) dupe = true;
+        for(int j = i + 1; j < size; j++){
+            if(values[i] == values[j]) duplicates = true;
         }
     }
-    return dupe;
+    return duplicates;
 }
